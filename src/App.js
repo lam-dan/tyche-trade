a228bc3a8c3a29eb139e4d6b31c1364909a06a92
Daniel <danlam@ucdavis.edu>
2019-08-20 02:24:44 -0700

    Added user authentication and creation in firebase for signing in

--- src/App.js
+++ src/App.js
@@ -1,42 +1,65 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
     super();
-
     this.state = {
       currentUser: null
     };
   }
-
   unsubscribeFromAuth = null;
 
-  //When we load the page, we have this open subscription
   componentDidMount() {
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      //Check to see if there is a user authorization when they log in
+      if (userAuth) {
+        //We store the user data in our database here and await
+        // for the returned user reference object getting sent back and store that in userRef
+        const userRef = await createUserProfileDocument(userAuth);
+        //We store this data in the state of our application so it can
+        // be used in our app
+        // We are going  to subscribe to that user reference and get back the first state of the data
+        userRef.onSnapshot(snapShot => {
+          console.log(snapShot);
+          // We are going to set state on that snapshot data
+          // setState is asynchronous so console log has to be in the callback
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      } else {
+        // If user ever logs out, we set the current state of the user to null, user autheticantion will be null
+        // in this case.
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
-  //close subscription when we close the window
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  
+
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -45,7 +68,6 @@ class App extends React.Component {
       </div>
     );
   }
-
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,25 +1,55 @@
-import firebase from 'firebase/app'
+import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyDsH14CQBSCngw1WRHVXdchyEHCFVGQBgI",
-    authDomain: "tyche-trade-db.firebaseapp.com",
-    databaseURL: "https://tyche-trade-db.firebaseio.com",
-    projectId: "tyche-trade-db",
-    storageBucket: "",
-    messagingSenderId: "116728161161",
-    appId: "1:116728161161:web:7bc4c7aee5000c37"
+  apiKey: 'AIzaSyDsH14CQBSCngw1WRHVXdchyEHCFVGQBgI',
+  authDomain: 'tyche-trade-db.firebaseapp.com',
+  databaseURL: 'https://tyche-trade-db.firebaseio.com',
+  projectId: 'tyche-trade-db',
+  storageBucket: '',
+  messagingSenderId: '116728161161',
+  appId: '1:116728161161:web:7bc4c7aee5000c37'
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  console.log(userAuth)
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  console.log(userRef)
+
+  const snapShot = await userRef.get();
+  console.log(snapShot)
+
+  //snapShot of the user authentication object does not exist 
+  if (!snapShot.exists) {
+    // we will create a new user with today's date
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    //We will set it on the document using the set method 
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
   }
+  // We return user reference once the document object has been created
+  return userRef;
+};
 
-  firebase.initializeApp(config);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-  const provider= new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt:'select_account'});
-  
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
-
-  export default firebase;
+export default firebase;
