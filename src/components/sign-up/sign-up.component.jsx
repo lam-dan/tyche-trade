bcb25314b59643eb0a0fa3f184fa3cff09d22da7
Daniel <danlam@ucdavis.edu>
2019-08-20 18:29:29 -0700

    Fixed CSS styling for sign-in-and-sign-up.component.jsx

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
@@ -13,29 +14,33 @@ class SignUp extends React.Component {
     this.state = {
       displayName: '',
       email: '',
-      password:'',
+      password: '',
       confirmPassword: ''
     };
   }
 
   handleSubmit = async event => {
     event.preventDefault();
+
     const { displayName, email, password, confirmPassword } = this.state;
+
     if (password !== confirmPassword) {
       alert("passwords don't match");
       return;
     }
+
     try {
       const { user } = await auth.createUserWithEmailAndPassword(
         email,
         password
       );
+
       await createUserProfileDocument(user, { displayName });
 
       this.setState({
         displayName: '',
         email: '',
-        password:'',
+        password: '',
         confirmPassword: ''
       });
     } catch (error) {
@@ -44,16 +49,16 @@ class SignUp extends React.Component {
   };
 
   handleChange = event => {
-      
-      const {name, value} = event.target;
-      this.setState({[name]:value})
-  }
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
 
   render() {
     const { displayName, email, password, confirmPassword } = this.state;
     return (
       <div className='sign-up'>
-        <h2 className='title'>I do not have an account</h2>
+        <h2 className='title'>I do not have a account</h2>
         <span>Sign up with your email and password</span>
         <form className='sign-up-form' onSubmit={this.handleSubmit}>
           <FormInput
@@ -88,7 +93,7 @@ class SignUp extends React.Component {
             label='Confirm Password'
             required
           />
-          <CustomButton type='submit'> SIGN UP</CustomButton>
+          <CustomButton type='submit'>SIGN UP</CustomButton>
         </form>
       </div>
     );
