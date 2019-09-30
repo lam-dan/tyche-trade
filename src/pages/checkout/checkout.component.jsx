c9ade6cf0e2ec7e04418272e6cd5c704c38cff7b
Daniel <danlam@ucdavis.edu>
2019-09-30 14:59:52 -0700

    Added checkout component

---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './checkout.styles.scss';
+// Basic functional component that just renders a div
+const CheckoutPage = () => (
+    <div>
+        CHECKOUT Page;
+    </div>
+)
+
+export default CheckoutPage;
