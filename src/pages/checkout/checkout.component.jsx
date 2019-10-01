b54ab4523842b0ae066175a588331abccaacedb5
Daniel <danlam@ucdavis.edu>
2019-10-01 00:04:36 -0700

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
+import {
+  selectCartItems,
+  selectCartTotal
+} from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 // Basic functional component that just renders a div
-const CheckoutPage = ({cartItems, total}) => (
+const CheckoutPage = ({ cartItems, total }) => (
   <div className='checkout-page'>
     <div className='checkout-header'>
       <div className='header-block'>
@@ -25,13 +30,11 @@ const CheckoutPage = ({cartItems, total}) => (
         <span>Remove</span>
       </div>
     </div>
-    {
-        cartItems.map(cartItem =>
-            cartItem.name
-            )
-    }
+    {cartItems.map(cartItem => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className='total'>
-        <span>TOTAL: ${total}</span>
+      <span>TOTAL: ${total}</span>
     </div>
   </div>
 );
