096026e32c6712849ec6dfd80af2054ffdf0858f
Daniel <danlam@ucdavis.edu>
2019-10-03 02:25:53 -0700

    Added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from '../cart/cart.utils'
+import { addItemToCart, removeItemFromCart } from '../cart/cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -8,6 +8,8 @@ const INITIAL_STATE = {
 
 // This is essentially a reducer listener that listens to the different action types
 // and then returns a new object with a new state
+
+
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
@@ -21,8 +23,22 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         // We are using a spread operator to take in give all the values of the state's cartItems array
         // and spreading them into this new array and also adding any additional values that might come in
         // action.payload
-        cartItems:addItemToCart(state.cartItems, action.payload)
-      }
+        cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+      case CartActionTypes.REMOVE_ITEM:
+        return {
+          ...state,
+          cartItems:removeItemFromCart(state.cartItems,action.payload)
+        }    
+
+
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+      };
     default:
       return state;
   }
